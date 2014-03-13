83fbb5b54945a2c13a3206fb70a963d65805cccd
rpletcher <rpletcher@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-13 04:25:31 +0000

    Fixing an error in spelling
    git-svn-id: https://plugins.svn.wordpress.org/gift-cards-for-woocommerce/trunk@874520 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- giftcard/giftcard-functions.php
+++ giftcard/giftcard-functions.php
@@ -25,7 +25,7 @@ function rpgc_create_number( $data , $postarr ) {
 		$data['post_name'] = $randomNumber;
 	}
 
-	return apply_filter('rpgc_create_number', $data);
+	return apply_filters('rpgc_create_number', $data);
 }
 add_filter( 'wp_insert_post_data' , 'rpgc_create_number' , '99', 2 );
 
